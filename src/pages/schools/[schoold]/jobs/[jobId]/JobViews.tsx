8e96cfa946549d5592902811a015bdc84cfe21fe
James Kaguru Kihuha <47499826+James-Kaguru@users.noreply.github.com>
2024-06-12 15:30:18 +0300

    removed unused filters

--- src/pages/schools/[schoold]/jobs/[jobId]/JobViews.tsx
+++ src/pages/schools/[schoold]/jobs/[jobId]/JobViews.tsx
@@ -1,14 +1,8 @@
-import filterList from "/icons/filter_list.svg";
-import search from "/icons/search.svg";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { DateTime } from "luxon";
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
+import { useState } from "react";
 import queryString from "query-string";
-import * as Popover from "@radix-ui/react-popover";
 import weteachApi from "../../../../../configs/weteach-api.ts";
 import PaginationSection from "../../../../../components/PaginationSection.tsx";
 import NoData from "../../../../../components/no-data.tsx";
@@ -40,36 +34,13 @@ function JobViewsTable({ jobViews }: { jobViews: JobView[] }) {
   );
 }
 
-const schema = z.object({
-  payment_rate: z.string().optional(),
-  teacher_requirements: z.string().optional(),
-  title: z.string().optional(),
-  status: z.string().optional(),
-});
-
-type ISchema = z.infer<typeof schema>;
-
 export default function JobViews() {
   const { jobId } = useParams();
   const [page, setPage] = useState(1);
 
-  const { register, watch, reset } = useForm<ISchema>({
-    resolver: zodResolver(schema),
-  });
-
-  useEffect(() => {
-    const subscription = watch(() => setPage(1));
-    return () => subscription.unsubscribe();
-  }, [watch]);
-
-  const status = watch("status");
-  const title = watch("title");
-
   const query = queryString.stringify(
     {
-      status,
       page,
-      title,
     },
     {
       skipEmptyString: true,
@@ -84,74 +55,8 @@ export default function JobViews() {
     placeholderData: (previousData) => previousData,
   });
 
-  function clearFilter() {
-    reset({
-      status: "",
-    });
-  }
-
   return (
     <section>
-      <div className={"flex flex-row items-center justify-between"}>
-        <div className={"flex flex-row gap-3"}>
-          <Popover.Root>
-            <Popover.Trigger asChild>
-              <button className="flex flex-row text-sm items-center gap-3 px-4 py-2  border border-gray-200 rounded-[32px]">
-                <img src={filterList} alt={"filter"} />
-                <p>Filter</p>
-              </button>
-            </Popover.Trigger>
-            <Popover.Portal>
-              <Popover.Content
-                className={"bg-white p-3 z-20 border border-gray-200 text-xs"}
-                side={"bottom"}
-                sideOffset={5}
-                align={"start"}
-              >
-                <div
-                  className={
-                    "flex flex-col gap-3 border-b border-gray-200 pb-3 mt-3"
-                  }
-                >
-                  <div
-                    className={
-                      "flex flex-row items-center w-full justify-center gap-3"
-                    }
-                  >
-                    <label className={"text-right text-xs"}>Status</label>
-
-                    <select {...register("status")} className={"p-2 text-xs"}>
-                      <option value={""}>Select status</option>
-                      <option value={"active"}>Active</option>
-                      <option value={"inactive"}>Inactive</option>
-                      <option value={"expired"}>Expired</option>
-                      <option value={"draft"}>Draft</option>
-                    </select>
-                  </div>
-                </div>
-
-                <button
-                  className={"w-full text-secondary text-sm pt-3"}
-                  onClick={() => clearFilter()}
-                >
-                  Clear filter
-                </button>
-              </Popover.Content>
-            </Popover.Portal>
-          </Popover.Root>
-
-          <label className="flex flex-row text-sm items-center gap-3 px-4 py-0 border border-gray-200 rounded-[32px] w-[300px]">
-            <img src={search} alt={"filter"} />
-            <input
-              {...register("title")}
-              type={"search"}
-              placeholder={"Search"}
-              className={"py-0  text-regular h-full border-0 focus:outline-0"}
-            />
-          </label>
-        </div>
-      </div>
-
       {data !== undefined ? (
         <>
           {data.data.results.length !== 0 ? (
